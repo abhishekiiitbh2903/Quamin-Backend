a4261a0ec1cc7b75c37ee520760b77db3e1c404c
Abhishek Singh <abhishekrathore1806.com>
2024-10-24 17:53:40 +0530

    Integrated JWT Authorisation

--- cors.py
+++ cors.py
@@ -1,14 +1,9 @@
 from fastapi.middleware.cors import CORSMiddleware
 
 def add_cors_middleware(app):
-    origins = [
-        "http://localhost:3000", 
-        "*"
-    ]
-
-    app.add_middleware(
+     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["http://localhost:3000"], 
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
